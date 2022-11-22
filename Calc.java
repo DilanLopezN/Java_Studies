97f4c744fbe6e3d0ebde562aa489fcdf89e3b830
Dilan Lopez <dilanlopez009@gmail.com>
2022-11-22 12:38:09 -0300

    first project simple calc

---
+++ Calc.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Calc {
+  public static void main(String[] args) {
+    Scanner entry = new Scanner(System.in);
+    System.out.print("Informe o número para calculo: ");
+    double numberForCalcOne = entry.nextDouble();
+
+    System.out.print("Informe o número para calculo: ");
+    double numberForCalcTwo = entry.nextDouble();
+
+    System.out.print("Informe a operação desejada: ");
+    String operation = entry.next();
+
+
+    double resultado = "+".equals(operation) ? numberForCalcOne + numberForCalcTwo : 0;
+    resultado = "-".equals(operation) ? numberForCalcOne - numberForCalcTwo : resultado;
+
+    resultado = "*".equals(operation) ? numberForCalcOne * numberForCalcTwo : resultado;
+
+    resultado = "/".equals(operation) ? numberForCalcOne / numberForCalcTwo : resultado;
+
+    resultado = "%".equals(operation) ? numberForCalcOne % numberForCalcTwo : resultado;
+
+
+
+    System.out.printf("%.2f %s %.2f = %.2f", numberForCalcOne, operation, numberForCalcTwo, resultado);
+    entry.close();
+  }
+}
